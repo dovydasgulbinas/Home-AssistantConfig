22b3bf611afa650965c172eb0f38c4842a7242a1
Hermes <dovydasgulbinas@gmail.com>
2017-05-31 17:50:27 +0300

    Adding debug scripts

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-cd /home/homeassistant/.homeassistant
-source /srv/homeassistant/homeassistant_venv/bin/activate
-hass --script check_config
+# cd /home/homeassistant/.homeassistant
+# source /srv/homeassistant/homeassistant_venv/bin/activate
+# hass --script check_config
+
+
+read -r -p "Do you wish to see git diff? [y/N]" response
+response=${response,,}    # tolower
+if [[ "$response" =~ ^(yes|y)$ ]]
+then
+    git diff
+else
+    "Skipping diff"
+fi
+
+echo "========================================"
 
 git add .
 git status
---
+++ hass-debugger.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd /home/homeassistant/.homeassistant
+source /srv/homeassistant/homeassistant_venv/bin/activate
+hass --script check_config
+
+# If you want to allow homeasistant to restart the service add line below to '/etc/sudoers' file
+# homeassistant	ALL=(ALL) NOPASSWD: /bin/systemctl restart home-assistant.service
+
+read -r -p "Do you wish to restart home-assistant.service? [y/N]" response
+response=${response,,}    # tolower
+if [[ "$response" =~ ^(yes|y)$ ]]
+then
+    echo "Restarting HASS"
+    sudo systemctl restart home-assistant.service
+else
+    echo "No Restart"
+fi
+
+read -r -p "Do you wish to read home assistant log? [y/N]" response
+response=${response,,}    # tolower
+if [[ "$response" =~ ^(yes|y)$ ]]
+then
+    tail -f home-assistant.log
+else
+    echo "No Log"
+fi
+
+exit
